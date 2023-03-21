5264cd84bceaee58dc60629d159d40a248166ea0
Donia El Fouly <donia.elfouly@gmail.com>
2023-03-21 22:11:30 +0200

    basic setup + implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Grid
+      templateAreas={{
+        base: `"nav""main"`,
+        lg: `"nav nav""side main"`,
+      }}
+      // gridTemplateRows={"50px 1fr 30px"}
+      // gridTemplateColumns={"150px 1fr"}
+      // h="200px"
+      // gap="1"
+      // color="blackAlpha.700"
+      // fontWeight="bold"
+    >
+      <GridItem pl="2" area={"nav"}>
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem pl="2" bg="pink.300" area={"side"}>
+          Side
+        </GridItem>
+      </Show>
 
-export default App
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
